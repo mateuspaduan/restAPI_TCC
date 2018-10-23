2be896cc259810c3e83ef2104adb267cc478c637
Pedro Zaroni <zaronipedro@outlook.com>
2018-10-22 22:12:43 -0300

    Fix session functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,19 +16,19 @@ exports.create_a_user = function (req, res) {
 
     User.find({ email: req.body.email }, function (err, user) {
         if (err) {
-            res.status(500).send('Ocorreu um erro ao cadastrar');
+            res.status(500).send({ message: 'Ocorreu um erro ao cadastrar' });
         }
         if (!user.length) {
             var new_user = new User(req.body);
 
             new_user.save(function (err, user) {
                 if (err)
-                    res.status(500).send('Não foi possível criar usuário.');
-                res.status(200).json(user);
+                    res.status(500).send({ message: 'Não foi possível criar usuário.' });
+                res.status(200).json(user._id);
             });
         }
         else {
-            res.status(500).send('Usuário já existe com esse email.');
+            res.status(500).json({ message: 'Usuário já existe com esse email.' });
         }
     })
 };
